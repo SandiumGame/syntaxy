2d55d67498c815f1520a7e1602a5312fb963588c
crankybunny <john_nospam@newonic.com>
2025-01-22 19:48:01 -0700

    Initial check-in

---
+++ backend/src/main/java/org/sandium/syntaxy/SampleCommon.java
@@ -0,0 +1,8 @@
+package org.sandium.syntaxy;
+
+public class SampleCommon {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ server/src/test/java/org/sandium/syntaxy/server/SyntaxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sandium.syntaxy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandiumServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
